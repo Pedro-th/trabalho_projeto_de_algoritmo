5d69d6663acb6e4d4ef855efe464e85e65ccd8cb
okamoto <123403861+okamoto-tomo@users.noreply.github.com>
2026-07-15 21:46:59 -0300

    fix

--- mergeSort/mergeSort.h
+++ mergeSort/mergeSort.h
@@ -2,5 +2,5 @@
 #include <vector>
 using namespace std;
 
-void mergeSort(vector <int> &vetor, int inicio, int fim, int meio, int &contador);
-void merge(vector <int> &vetor,int &contador);
+void mergeSort(vector <int> &vetor, int inicio, int fim, int &contador);
+void merge(vector <int> &vetor, int inicio, int fim, int meio, int &contador);
