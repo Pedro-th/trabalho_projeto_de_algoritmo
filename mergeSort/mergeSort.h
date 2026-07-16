f01046ee7bc0206e801a77f916963037f8b267e4
okamoto <123403861+okamoto-tomo@users.noreply.github.com>
2026-07-15 21:45:58 -0300

    fix: <double> para <int>

--- mergeSort/mergeSort.h
+++ mergeSort/mergeSort.h
@@ -2,5 +2,5 @@
 #include <vector>
 using namespace std;
 
-void mergeSort(vector <double> &vetor, int inicio, int fim, int meio, int &contador);
-void merge(vector <double> &vetor,int &contador);
+void mergeSort(vector <int> &vetor, int inicio, int fim, int meio, int &contador);
+void merge(vector <int> &vetor,int &contador);
